dc843f4cfadd5e32920738f9786221312ee65062
priya270497 <131447325+priya270497@users.noreply.github.com>
2023-06-12 12:31:20 +0530

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+    1: {"author": "Chinua Achebe","title": "Things Fall Apart", "isbn": 1,"reviews": {} },
+    2: {"author": "Hans Christian Andersen","title": "Fairy tales", "isbn": 2,"reviews": {} },
+    3: {"author": "Dante Alighieri","title": "The Divine Comedy","isbn": 3, "reviews": {} },
+    4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "isbn": 4,"reviews": {} },
+    5: {"author": "Unknown","title": "The Book Of Job", "isbn": 5,"reviews": {} },
+    6: {"author": "Unknown","title": "One Thousand and One Nights", "isbn": 6,"reviews": {} },
+    7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "isbn": 7,"reviews": {} },
+    8: {"author": "Jane Austen","title": "Pride and Prejudice", "isbn": 8,"reviews": {} },
+    9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "isbn": 9, "reviews": {} },
+    10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "isbn": 10,"reviews": {} }
 }
 
 module.exports=books;
